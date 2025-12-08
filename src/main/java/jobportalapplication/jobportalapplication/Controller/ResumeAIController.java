db2fa3891c44df3eb5446e2eb011c2c44d083297
Rajsingh6389 <rm2739159@gmail.com>
2025-12-08 17:48:50 +0530

    added chat

--- src/main/java/jobportalapplication/jobportalapplication/Controller/ResumeAIController.java
+++ src/main/java/jobportalapplication/jobportalapplication/Controller/ResumeAIController.java
@@ -5,6 +5,7 @@ import jobportalapplication.jobportalapplication.dto.AIPromptRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/ai/resume")
